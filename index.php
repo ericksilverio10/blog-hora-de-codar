f84d5d1eb6a0f67512e27947aa74b993338e1d0a
Erick <erick1junior@hotmail.com>
2022-04-13 10:04:37 -0300

    Create Header CSS

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    include_once('templates/header.php');
+?>
+<h1>
+    Olá mundo, aqui é a página principal
+</h1>
+<?php
+    include_once('templates/footer.php');
+?>
---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+        <footer>
+            <p>Hora de Codar &copy</p>
+        </footer>
+    </body>
+</html>
